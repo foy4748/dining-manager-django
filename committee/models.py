d06745d841c4fa372d07059301acd64e015ca38b
f-rahman23 <f.rahman@iideainformatics.com>
2023-07-07 11:44:38 +0600

    Added Committe Model

---
+++ committee/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class COMMITTEE(models.Model):
+    committee_no = models.IntegerField(null=False, blank=False)
+    meal_rate = models.FloatField(null=False, blank=False)
+
+    def __str__(self):
+        return "Committe no. {a}".format(self.committee_no)

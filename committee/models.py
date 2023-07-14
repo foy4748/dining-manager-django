bdbb04964faa72c95f7f191b1ee4ca4f6f06f49e
f-rahman23 <f.rahman@iideainformatics.com>
2023-07-15 01:13:09 +0600

    Limited the scope of uniqueness to a single committe

--- committee/models.py
+++ committee/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class COMMITTEE(models.Model):
     committee_no = models.IntegerField(null=False, blank=False)
     meal_rate = models.FloatField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.committee_no)
--- request/serializers.py
+++ request/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import  serializers
 from .models import ACTIVATION_REQUEST, DEACTIVATION_REQUEST
+from committee.models import COMMITTEE
 
 from rest_framework.validators import UniqueForDateValidator
 
@@ -9,7 +10,7 @@ class ActivationRequestSerializer(serializers.ModelSerializer):
         fields = '__all__'
         # Preventing Same Date Activation Posting
         validators = [
-                UniqueForDateValidator(queryset=ACTIVATION_REQUEST.objects.all(), field="user_id", date_field="activation_date")
+                UniqueForDateValidator(queryset=ACTIVATION_REQUEST.objects.filter(committee_id=COMMITTEE.objects.latest('created_at')), field="user_id", date_field="activation_date")
         ]
 
 class DeactivationRequestSerializer(serializers.ModelSerializer):
@@ -17,6 +18,6 @@ class DeactivationRequestSerializer(serializers.ModelSerializer):
         model = DEACTIVATION_REQUEST
         fields = '__all__'
         validators = [
-                UniqueForDateValidator(queryset=DEACTIVATION_REQUEST.objects.all(), field="user_id", date_field="deactivation_start_date")
+                UniqueForDateValidator(queryset=DEACTIVATION_REQUEST.objects.filter(committee_id=COMMITTEE.objects.latest('created_at')), field="user_id", date_field="deactivation_start_date")
         ]
 

6be0cbebc969861902d1e998ad9a24a8fd236acc
f-rahman23 <f.rahman@iideainformatics.com>
2023-07-15 00:58:49 +0600

    Prevented Same Date Activation/Deactivation Request for a same user
    Thanks to doc page
    https://www.django-rest-framework.org/api-guide/validators/#uniquefordatevalidator

--- request/serializers.py
+++ request/serializers.py
@@ -1,13 +1,22 @@
 from rest_framework import  serializers
 from .models import ACTIVATION_REQUEST, DEACTIVATION_REQUEST
 
+from rest_framework.validators import UniqueForDateValidator
+
 class ActivationRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = ACTIVATION_REQUEST
         fields = '__all__'
+        # Preventing Same Date Activation Posting
+        validators = [
+                UniqueForDateValidator(queryset=ACTIVATION_REQUEST.objects.all(), field="user_id", date_field="activation_date")
+        ]
 
 class DeactivationRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = DEACTIVATION_REQUEST
         fields = '__all__'
+        validators = [
+                UniqueForDateValidator(queryset=DEACTIVATION_REQUEST.objects.all(), field="user_id", date_field="deactivation_start_date")
+        ]
 

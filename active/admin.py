e7b07fc90b87f47e8a6216036a91c90017b225db
f-rahman23 <f.rahman@iideainformatics.com>
2023-07-17 01:23:58 +0600

    Registered those two models in Admin panel

--- active/admin.py
+++ active/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import ACTIVE_MEAL
+
 # Register your models here.
+admin.site.register(ACTIVE_MEAL)
--- counter/admin.py
+++ counter/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import MEAL_COUNTER
 # Register your models here.
+
+admin.site.register(MEAL_COUNTER)
